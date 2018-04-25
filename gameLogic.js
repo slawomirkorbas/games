477b7aac581a221fe29e79d3c0c3f3b12e0edf7a
slawomirkorbas <slawomirkorbas@gmail.com>
2018-04-25 14:38:46 +0200

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -51,7 +51,7 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
     else if( gameResult == COMPUTER_WIN ) {
         if( movesCount == 3 ) {
             // 3 means computer is able to win in the next move - this should be recommended game solution so we give it +10
-            evaluation.pts += 100;
+            evaluation.pts += (1 + emptyFieldsLeft(matrix));
         }
         else {
             evaluation.pts += (1 + emptyFieldsLeft(matrix));
